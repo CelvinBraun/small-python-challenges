5c3b478d480dd0dfefb3590387b9416e5f226067
Celvin Braun <46591058+CelvinBraun@users.noreply.github.com>
2021-12-15 00:09:21 +0100

    quick bug fix

--- geschenke_zip_challenge/main.py
+++ geschenke_zip_challenge/main.py
@@ -5,19 +5,17 @@ file_name = "geschenk.zip"
 path = "data/"
 job_done = False
 
+with ZipFile(path + file_name, 'r') as zip:
+    # printing all the contents of the zip file
+    zip.printdir()
+
+    # extracting all the files
+    print('Extracting files')
+    zip.extractall(path)
+    print('Done!\n')
 
 #run until all files are unzipped
 while job_done != True:
-
-    #unzip file
-    with ZipFile(path + file_name, 'r') as zip:
-        # printing all the contents of the zip file
-        zip.printdir()
-
-        # extracting all the files
-        print('Extracting files')
-        zip.extractall(path)
-        print('Done!\n')
 
     pathContent = os.listdir(path)
 
@@ -25,6 +23,16 @@ while job_done != True:
         pathContent.remove(file_name)
         os.remove(path+file_name)
         file_name = pathContent[0]
+
+        # unzip file
+        with ZipFile(path + file_name, 'r') as zip:
+            # printing all the contents of the zip file
+            zip.printdir()
+
+            # extracting all the files
+            print('Extracting files')
+            zip.extractall(path)
+            print('Done!\n')
 
     else:
         job_done = True
